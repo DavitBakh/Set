388d5522346fc23da797fd4863c63c8da24d91dc
DavitBakh <davobakhshyan@gmail.com>
2024-11-24 20:33:06 +0400

    Test 6

--- Set/Set.cpp
+++ Set/Set.cpp
@@ -1,16 +1,17 @@
 #include <cassert>
 #include <queue>
+#include <vector>
 #include "Set.h"
 
-Set::Set() : _size(0), _root(nullptr) { }
+Set::Set() : _size(0), _root(nullptr) {}
 
-Set::Set(std::initializer_list<int> initList) : _size(0), _root(nullptr)
+Set::Set(std::initializer_list<int> initList) : Set::Set()
 {
 	for (auto& i : initList)
 		insert(i);
 }
 
-Set::Set(const Set& source)
+Set::Set(const Set& source) : Set::Set()
 {
 	std::queue<Node*> queue;
 	queue.push(source._root);
@@ -33,6 +34,17 @@ Set::Set(const Set& source)
 
 Set::Node::Node(int val = 0, Node* parent = nullptr, Node* left = nullptr, Node* right = nullptr) : _val(val), _parent(parent), _left(left), _right(right) {}
 
+Set::Node::~Node()
+{
+	delete _right;
+	delete _left;
+}
+
+Set::~Set()
+{
+	delete _root;
+}
+
 
 size_t Set::size()
 {
@@ -43,6 +55,104 @@ bool Set::empty()
 {
 	return _size == 0;
 }
+
+
+void Set::insert(int val)
+{
+	Node* parent = nullptr;
+	Node*& res = find(_root, val, parent);
+	if (res == nullptr)
+	{
+		res = new Node(val, parent);
+		++_size;
+	}
+}
+
+bool Set::contains(int val)
+{
+	return find(_root, val) != nullptr;
+}
+
+
+void Set::erase(int val)
+{
+	Node* node = find(_root, val);
+
+	if (node == nullptr)
+		return;
+
+	if (node->_left == nullptr && node->_right == nullptr)
+		remove_leaf(node);
+	else if (node->_left == nullptr || node->_right == nullptr)
+		remove_1_child_node(node);
+	else
+		remove_2_child_node(node);
+
+	--_size;
+}
+
+Set& Set::operator=(const Set& source)
+{
+	if (&source != this)
+	{
+		this->~Set();
+
+		std::queue<Node*> queue;
+		queue.push(source._root);
+
+		Node* curr;
+		while (!queue.empty())
+		{
+			curr = queue.front();
+			queue.pop();
+
+			if (curr == nullptr)
+				continue;
+
+			queue.push(curr->_left);
+			queue.push(curr->_right);
+
+			insert(curr->_val);
+		}
+	}
+
+	return *this;
+}
+
+bool operator==(const Set& lhs, const Set& rhs)
+{
+	if (lhs._size != rhs._size)
+		return false;
+
+	Set::Node* curr1 = lhs._root;
+	Set::Node* curr2 = rhs._root;
+
+	while (curr1->_left != nullptr)
+		curr1 = curr1->_left;
+
+	while (curr2->_left != nullptr)
+		curr2 = curr2->_left;
+
+	while (curr1 != nullptr)
+	{
+		if (curr1->_val != curr2->_val)
+			return false;
+
+		curr1 = Set::inorder_next(curr1);
+		curr2 = Set::inorder_next(curr2);
+	}
+
+	return true;
+}
+
+bool operator!=(const Set& lhs, const Set& rhs)
+{
+	return !(lhs == rhs);
+}
+
+
+
+#pragma region Privat Functions
 
 Set::Node*& Set::find(Node*& node, int& val)
 {
@@ -65,22 +175,6 @@ Set::Node*& Set::find(Node*& node, int& val, Node*& outParent)
 		return find(node->_left, val, outParent);
 
 	return find(node->_right, val, outParent);
-}
-
-void Set::insert(int val)
-{
-	Node* parent = nullptr;
-	Node*& res = find(_root, val, parent);
-	if (res == nullptr)
-	{
-		res = new Node(val, parent);
-		++_size;
-	}
-}
-
-bool Set::contains(int val)
-{
-	return find(_root, val) != nullptr;
 }
 
 void Set::remove_leaf(Node* node)
@@ -112,6 +206,11 @@ void Set::remove_1_child_node(Node* node)
 		else
 			node->_parent->_right = c;
 	}
+
+	if (has_right)
+		node->_right = nullptr;
+	else
+		node->_left = nullptr;
 
 	c->_parent = node->_parent;
 	delete node;
@@ -156,21 +255,37 @@ void Set::remove_2_child_node(Node* node)
 		remove_1_child_node(curr);
 }
 
-void Set::erase(int val)
+Set::Node* Set::inorder_next(Node* node)
 {
-	Node* node = find(_root, val);
-
-	if (node == nullptr)
-		return;
-
-	if (node->_left == nullptr && node->_right == nullptr)
-		remove_leaf(node);
-	else if (node->_left == nullptr || node->_right == nullptr)
-		remove_1_child_node(node);
-	else
-		remove_2_child_node(node);
-
-	--_size;
+	if (node->_right != nullptr) {
+		node = node->_right;
+		while (node->_left != nullptr)
+			node = node->_left;
+		return node;
+	}
+	else {
+		while (node->_parent != nullptr && node == node->_parent->_right)
+			node = node->_parent;
+		return node->_parent;
+	}
 }
 
+Set::Node* Set::inorder_prev(Node* node)
+{
+	if (node->_left != nullptr) {
 
+		node = node->_left;
+		while (node->_right != nullptr)
+			node = node->_right;
+		return node;
+	}
+	else {
+
+		while (node->_parent != nullptr && node == node->_parent->_left)
+			node = node->_parent;
+		return node->_parent;
+	}
+	return nullptr;
+}
+
+#pragma endregion
--- Set/Set.h
+++ Set/Set.h
@@ -14,7 +14,7 @@ class Set
 		Node* _parent;
 
 		Node(int val, Node* parent, Node* left, Node* right);
-		//~Node(); ???
+		~Node();
 
 	};
 
@@ -28,12 +28,15 @@ class Set
 	void remove_leaf(Node* node);
 	void remove_1_child_node(Node* node);
 	void remove_2_child_node(Node* node);
+	static Node* inorder_next(Node* node);
+	static Node* inorder_prev(Node* node);
 
 public:
 
 	Set();
 	Set(const Set& source);
 	Set(std::initializer_list<int> initList);
+	~Set();
 
 	size_t size();
 	bool empty();
@@ -42,4 +45,8 @@ public:
 	void insert(int val);
 	bool contains(int val);
 	void erase(int val);
+
+	Set& operator=(const Set& source);
+	friend bool operator==(const Set& lhs, const Set& rhs);
+
 };
